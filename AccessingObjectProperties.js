62024f10099ed7b9a3380a9057fd47ae6a08b3d5
Denise <meagher.denise@gmail.com>
2016-11-15 16:45:03 -0500

    :Accessing Object Properties 03

--- AccessingObjectProperties.js
+++ AccessingObjectProperties.js
@@ -14,6 +14,7 @@ var shirtValue = testObj.shirt; // Change this line
 
 
 //02 /* The second way to access the properties of an object is bracket notation ([]). If the property of the object you are trying to access has a space in it, you will need to use bracket notation. */
+//setup
 var testObj = {
   "an entree": "hamburger",
   "my side": "veggies",
@@ -22,3 +23,17 @@ var testObj = {
 
 var entreeValue = testObj["an entree"];   // Change this line
 var drinkValue = testObj["the drink"];   // Change this line
+
+
+//03 /* Another use of bracket notation on objects is to use a variable to access a property. This can be very useful for iterating through lists of the object properties or for doing the lookup. */
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;                 // Change this Line
+var player = testObj[playerNumber];   // Change this Line
