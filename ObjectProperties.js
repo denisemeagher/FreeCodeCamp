2695cc033be19073f334c7a3cc89f2fa751ff2ff
Denise <meagher.denise@gmail.com>
2016-11-15 17:17:25 -0500

    edit file name and setup

--- ObjectProperties.js
+++ ObjectProperties.js
@@ -1,5 +1,18 @@
+//222 - Objects
 
-//01 //* Setup  Read in the property values of testObj using dot notation.
+//01
+/* Make an object that represents a dog called myDog which contains the properties "name" (a string), "legs", "tails" and "friends". */
+
+var myDog = {
+  "name": "Kiwi",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["ALL of the friends!"]
+};
+
+
+//02
+//* Setup  Read in the property values of testObj using dot notation.
 Set the variable hatValue equal to the object property hat and set the
 variable shirtValue equal to the object property shirt. */
 var testObj = {
@@ -11,7 +24,8 @@ var hatValue = testObj.hat;      // Change this line
 var shirtValue = testObj.shirt; // Change this line
 
 
-//02 /* The second way to access the properties of an object is bracket notation ([]). If the property of the object you are trying to access has a space in it, you will need to use bracket notation. */
+//03
+/* The second way to access the properties of an object is bracket notation ([]). If the property of the object you are trying to access has a space in it, you will need to use bracket notation. */
 //setup
 var testObj = {
   "an entree": "hamburger",
@@ -23,7 +37,8 @@ var entreeValue = testObj["an entree"];   // Change this line
 var drinkValue = testObj["the drink"];   // Change this line
 
 
-//03 /* Another use of bracket notation on objects is to use a variable to access a property. This can be very useful for iterating through lists of the object properties or for doing the lookup. */
+//04
+/* Another use of bracket notation on objects is to use a variable to access a property. This can be very useful for iterating through lists of the object properties or for doing the lookup. */
 // Setup
 var testObj = {
   12: "Namath",
@@ -35,7 +50,8 @@ var playerNumber = 16;                 // Change this Line
 var player = testObj[playerNumber];   // Change this Line
 
 
-//04 /* Update the myDog object's name property. Let's change her name from "Coder" to "Happy Coder". You can use either dot or bracket notation. */
+//05
+/* Update the myDog object's name property. Let's change her name from "Coder" to "Happy Coder". You can use either dot or bracket notation. */
 
 // Setup
 var myDog = {
