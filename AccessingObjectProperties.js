3ab3817d9e7ba60e5d83cc26492ef90da6e7a4e9
Denise <meagher.denise@gmail.com>
2016-11-15 16:08:52 -0500

    :Accessing Object Properties 02

--- AccessingObjectProperties.js
+++ AccessingObjectProperties.js
@@ -1,7 +1,5 @@
-//01
 
-
-/* Setup  Read in the property values of testObj using dot notation.
+//01 //* Setup  Read in the property values of testObj using dot notation.
 Set the variable hatValue equal to the object property hat and set the
 variable shirtValue equal to the object property shirt. */
 
@@ -11,8 +9,16 @@ var testObj = {
   "shoes": "cleats"
 };
 
-var hatValue = testObj.hat;       // ballcap
-var shirtValue = testObj.shirt;  // jersey
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt; // Change this line
 
 
-//01
+//02 /* The second way to access the properties of an object is bracket notation ([]). If the property of the object you are trying to access has a space in it, you will need to use bracket notation. */
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];   // Change this line
