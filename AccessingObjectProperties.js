56b89b3770a0da5c844f0ea8d5b13d0750600b64
Denise <meagher.denise@gmail.com>
2016-11-15 14:49:09 -0500

    :Accessing Object Properties 01

---
+++ AccessingObjectProperties.js
@@ -0,0 +1,18 @@
+//01
+
+
+/* Setup  Read in the property values of testObj using dot notation.
+Set the variable hatValue equal to the object property hat and set the
+variable shirtValue equal to the object property shirt. */
+
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+var hatValue = testObj.hat;       // ballcap
+var shirtValue = testObj.shirt;  // jersey
+
+
+//01
